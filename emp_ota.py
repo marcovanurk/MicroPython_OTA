396a566578da87d519496aebf40a2661f2cef52d
Marco <marcovanurk@users.noreply.github.com>
2023-08-23 12:42:47 +0200

    Update emp_ota.py

--- emp_ota.py
+++ emp_ota.py
@@ -1,1 +1,10 @@
+from ota import OTAUpdater
+from WIFI_CONFIG import SSID, PASSWORD
+
+firmware_url = "https://raw.githubusercontent.com/marcovanurk/MicroPython_OTA/"
+
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "emp_ota.py")
+ota_updater.download_and_install_update_if_available()
+
+
 print ('Hello World v1')
