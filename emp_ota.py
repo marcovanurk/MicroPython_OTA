8cfbe0d57a6c537af6ba52110c4e9a4953d63fc8
Marco <marcovanurk@users.noreply.github.com>
2023-08-23 12:45:50 +0200

    Update emp_ota.py

--- emp_ota.py
+++ emp_ota.py
@@ -3,8 +3,10 @@ from WIFI_CONFIG import SSID, PASSWORD
 
 firmware_url = "https://raw.githubusercontent.com/marcovanurk/MicroPython_OTA/"
 
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "emp_ota.py")
-ota_updater.download_and_install_update_if_available()
-
-
-print ('Hello World v1')
+while True:
+    print ('Hello World v2')
+    time.sleep(2500)
+    
+    // check new version available
+    ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "emp_ota.py")
+    ota_updater.download_and_install_update_if_available()
