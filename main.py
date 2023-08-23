28fa2d5b418297008f2499e6a667549d3bf5ede2
Marco <marcovanurk@users.noreply.github.com>
2023-08-23 15:56:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ displayText = '               '
 #
 while True:
     display.text(displayText, 0, 0, 0)
-    displayText = "version 1.1"
+    displayText = "version 1.2"
     display.text(displayText, 0, 0, 1)
     display.show()
     
