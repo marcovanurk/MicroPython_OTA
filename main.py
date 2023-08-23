32457f4f7054b0846cf45aea50443698f1d11639
Marco <marcovanurk@users.noreply.github.com>
2023-08-23 15:28:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import time
 import machine
+from machine import Pin, I2C
 
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
+
+import ssd1306
 
 
 
@@ -15,20 +18,30 @@ def GetNewVersion():
     ota_updater.download_and_install_update_if_available()
 
 
-led = machine.Pin("LED", machine.Pin.OUT) # 25 is 'Pico' and "LED" is 'Pico W'
+
+led = Pin("LED", Pin.OUT) # 25 is 'Pico' and "LED" is 'Pico W'
 firmware_url = "https://raw.githubusercontent.com/marcovanurk/MicroPython_OTA/"
 
 
+# using default address 0x3C
+i2c = I2C(0, sda=Pin(12), scl=Pin(13))
+display = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 #
 # loop
 #
 while True:
-    for i in range(0, 10):
-        led.toggle()
-        time.sleep(0.25)
-        
-    GetNewVersion()
+    displayText = "version 1.0"
+    display.text(displayText, 0, 0, 1)
+    display.show()
     
-    machine.reset()
+    for i in range(30, 0, -1):
+        display.text(str(i), 63, 32, 1)
+        display.show()
+        time.sleep(1.0)
+        display.text(str(i), 63, 32, 0)
+        
+    #GetNewVersion()
+    
+    #machine.reset()
 
