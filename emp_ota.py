659b431a8d49c358a027a3339262cfb6da6f6f63
Marco <marcovanurk@users.noreply.github.com>
2023-08-23 13:05:07 +0200

    Update emp_ota.py

--- emp_ota.py
+++ emp_ota.py
@@ -1,15 +1,31 @@
 import time
+from machine import Pin
+
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 
+
+
+#
+#
+#
+def GetNewVersion():
+    # check new version available
+    ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "emp_ota.py")
+    ota_updater.download_and_install_update_if_available()
+
+
+led = Pin("LED", Pin.OUT) # 25 is 'Pico' and "LED" is 'Pico W'
 firmware_url = "https://raw.githubusercontent.com/marcovanurk/MicroPython_OTA/"
 
 
 
+#
+# loop
+#
 while True:
-    print ('\n\nHello World v4\n\n')
-    time.sleep(2.5)
-    
-    # check new version available
-    ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "emp_ota.py")
-    ota_updater.download_and_install_update_if_available()
+    for i in range(0, 10):
+        led.toggle()
+        time.sleep(.25)
+        
+    GetNewVersion()
